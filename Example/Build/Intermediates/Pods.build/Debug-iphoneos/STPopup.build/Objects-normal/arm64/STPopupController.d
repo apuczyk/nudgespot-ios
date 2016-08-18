598f428c8555f643ac37ababbece6592db4b2914
Maheep Kaushal <maheep@nudgespot.com>
2016-08-18 16:08:12 +0530

    Update Nudgespot Fcm

--- Example/Build/Intermediates/Pods.build/Debug-iphoneos/STPopup.build/Objects-normal/arm64/STPopupController.d
+++ Example/Build/Intermediates/Pods.build/Debug-iphoneos/STPopup.build/Objects-normal/arm64/STPopupController.d
@@ -5,4 +5,6 @@ dependencies: \
   /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/STPopupNavigationBar.h \
   /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/STPopupLeftBarItem.h \
   /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/UIViewController+STPopup.h \
-  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/UIResponder+STPopup.h
+  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/UIResponder+STPopup.h \
+  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/STPopupControllerTransitioningSlideVertical.h \
+  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/STPopupControllerTransitioningFade.h
---
+++ Example/Build/Intermediates/Pods.build/Debug-iphoneos/STPopup.build/Objects-normal/arm64/STPopupControllerTransitioningFade.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/STPopupControllerTransitioningFade.m \
+  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/Target\ Support\ Files/STPopup/STPopup-prefix.pch \
+  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/STPopupControllerTransitioningFade.h \
+  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/STPopupController.h \
+  /Users/nudgespot/Desktop/Nudgespot/NudgespotSDK/nudgespot-ios/Example/Pods/STPopup/STPopup/STPopupNavigationBar.h
