7390c6b3f995a4fac32a15da60ce88e6798e120c
Maheep Kaushal <maheep@nudgespot.com>
2016-09-26 14:31:34 +0530

    Initial commit

---
+++ Example/Nudgespot/NSViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSViewController.h
+//  Nudgespot
+//
+//  Created by Maheep Kaushal on 09/26/2016.
+//  Copyright (c) 2016 Maheep Kaushal. All rights reserved.
+//
+
+@import UIKit;
+
+@interface NSViewController : UIViewController
+
+@end
---
+++ Example/Nudgespot/NSViewController.m
@@ -0,0 +1,29 @@
+//
+//  NSViewController.m
+//  Nudgespot
+//
+//  Created by Maheep Kaushal on 09/26/2016.
+//  Copyright (c) 2016 Maheep Kaushal. All rights reserved.
+//
+
+#import "NSViewController.h"
+
+@interface NSViewController ()
+
+@end
+
+@implementation NSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
