95e01f31f6850cda9d4f4403eb47f27edfa97e26
Shree Madhu Krishna Murthy <madhu@boomtrain.com>
2017-04-07 17:18:48 +0530

    Removing the firebase dependency from podspec and a new version release

--- Example/Nudgespot/Comman/NudgeSpotConstants.h
+++ Example/Nudgespot/Comman/NudgeSpotConstants.h
@@ -11,12 +11,10 @@
 
 // To get these access keys go to https://app.nudgespot.com Settings page and Press Advance option and then check Access keys
 
-#define kJavascriptAPIkey @"cd3fcd5c9e5adce966b47c875298e9ea"
-
-#define kRESTAPIkey @"7e8b2d22a6748503864177c86d6bfd9b"
+// Preprod madhu@nudgespot.com
+#define kJavascriptAPIkey @"e4b4048df438ee912b54adb49cc700ca"
+#define kRESTAPIkey @"cc2454ad148608657deba8af89a0aff4"
 
 // LOCALHOST
-
 //#define kJavascriptAPIkey @"d3c75259d173ceb87fb2dcc99ba8b3dd"
-//
 //#define kRESTAPIkey @"754aec3365d22a487196f1b3201e42bc"
