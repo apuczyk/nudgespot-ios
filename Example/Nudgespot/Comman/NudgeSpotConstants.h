fa6b887e30c3e546484ebde1aa37059649b6716e
Shree Madhu Krishna Murthy <madhu@boomtrain.com>
2017-08-23 16:59:40 +0530

    Updated example proj with latest sdk version

--- Example/Nudgespot/Comman/NudgeSpotConstants.h
+++ Example/Nudgespot/Comman/NudgeSpotConstants.h
@@ -11,9 +11,13 @@
 
 // To get these access keys go to https://app.nudgespot.com Settings page and Press Advance option and then check Access keys
 
+// Prod madhu+bme@boomtrain.com
+#define kJavascriptAPIkey @"e64c6b4f06a84ebbba451278acbce284"
+#define kRESTAPIkey @"5532481fc515cbfbc77d1a8708a15627"
+
 // Preprod madhu@nudgespot.com
-#define kJavascriptAPIkey @"e4b4048df438ee912b54adb49cc700ca"
-#define kRESTAPIkey @"cc2454ad148608657deba8af89a0aff4"
+//#define kJavascriptAPIkey @"e4b4048df438ee912b54adb49cc700ca"
+//#define kRESTAPIkey @"cc2454ad148608657deba8af89a0aff4"
 
 // LOCALHOST
 //#define kJavascriptAPIkey @"d3c75259d173ceb87fb2dcc99ba8b3dd"
